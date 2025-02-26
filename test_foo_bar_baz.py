348439affb0ad6596c8b00dea498351385289da1
Qiyuan Tan <119595547+QiyuanTan@users.noreply.github.com>
2025-02-26 15:20:26 -0800

    Implemented test_foo_bar_baz.py and generated test report

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -3,3 +3,26 @@ import pytest
 from foo_bar_baz import foo_bar_baz
 
 #Add testcases Here
+def test_foo_bar_baz():
+    assert foo_bar_baz(0) == ""
+    assert foo_bar_baz(1) == "1"
+    assert foo_bar_baz(2) == "1 2"
+    assert foo_bar_baz(3) == "1 2 Foo"
+    assert foo_bar_baz(5) == "1 2 Foo 4 Bar"
+    assert foo_bar_baz(15) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz"
+
+
+def test_foo_bar_baz_negative():
+    assert foo_bar_baz(-2) == "0 -1 -2"
+    assert foo_bar_baz(-3) == "0 -1 -Foo"
+
+def test_foo_bar_baz_non_int():
+    try:
+        assert foo_bar_baz(1.1) == ""
+    except TypeError:
+        pass
+
+    try:
+            assert foo_bar_baz("some random string") == ""
+    except TypeError:
+        pass
